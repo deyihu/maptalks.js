9e4e0c979352e68c558cc2584bf2ce8d74840e1d
deyihu <mehudeyi@163.com>
2021-08-31 13:51:55 +0800

    fix map.animateTo framer

--- src/map/Map.Anim.js
+++ src/map/Map.Anim.js
@@ -2,7 +2,7 @@ import { Animation } from '../core/Animation';
 import Coordinate from '../geo/Coordinate';
 import Point from '../geo/Point';
 import Map from './Map';
-import { isNil, isFunction, hasOwn, extend, clamp, now } from '../core/util';
+import { isNil, isFunction, hasOwn, extend, clamp } from '../core/util';
 
 // function equalView(view1, view2) {
 //     for (const p in view1) {
@@ -90,16 +90,16 @@ Map.include(/** @lends Map.prototype */{
         }
         const zoomOrigin = view['around'] || new Point(this.width / 2, this.height / 2);
         // let preView = this.getView();
-        const renderer = this._getRenderer(),
-            framer = function (fn) {
-                renderer.renderFrame(now());
-                renderer.callInNextFrame(fn);
-            };
+        // const renderer = this._getRenderer(),
+        //     framer = function (fn) {
+        //         renderer.renderFrame(now());
+        //         renderer.callInNextFrame(fn);
+        //     };
 
         const player = this._animPlayer = Animation.animate(props, {
             'easing': options['easing'] || 'out',
-            'duration': options['duration'] || this.options['zoomAnimationDuration'],
-            'framer': framer
+            'duration': options['duration'] || this.options['zoomAnimationDuration']
+            // 'framer': framer
         }, frame => {
             if (this.isRemoved()) {
                 player.finish();
