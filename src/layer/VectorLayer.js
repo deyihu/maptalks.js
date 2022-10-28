a0ac3256a19b16a63edc81c66330b30e3b4b07aa
deyihu <mehudeyi@163.com>
2022-10-28 16:11:54 +0800

    fix geometry _hitTestTolerance is null

--- src/layer/VectorLayer.js
+++ src/layer/VectorLayer.js
@@ -127,7 +127,7 @@ class VectorLayer extends OverlayLayer {
         if (imageData) {
             let hitTolerance = 0;
             for (let i = geometries.length - 1; i >= 0; i--) {
-                if (!geometries[i]) {
+                if (!geometries[i] || !geometries[i]._hitTestTolerance) {
                     continue;
                 }
                 const t = geometries[i]._hitTestTolerance() + (tolerance || 0);
